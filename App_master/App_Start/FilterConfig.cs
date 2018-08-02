4b4da90fe0429e62606e65555eea2010a6e0c7cc
syedhassanali203 <syedhassanali203@gmail.com>
2018-08-02 21:01:42 +0500

    Add project files.

---
+++ App_master/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace App_master
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ App_master/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="App_master.MvcApplication" Language="C#" %>
